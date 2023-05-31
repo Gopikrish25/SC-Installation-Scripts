3695082ab3782823b644e66438a4128bc4b0f7ea
Sanka Gopi Krishna <55784517+Gopikrish25@users.noreply.github.com>
2023-05-31 20:20:01 +0530

    Add files via upload

---
+++ MMA.ps1
@@ -0,0 +1,23 @@
+﻿[CmdletBinding()]
+param(
+
+    [string] $AuthMode = 'Mixed',
+    [string] $Server
+)
+
+
+# Connect to the local SQL instance using SMO
+$Server = hostname
+# Comment the addition of \SQLEXPRESS if you are not using a named instance
+$Server += '\SQLEXPRESS'
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO') | out-null
+$s = new-object('Microsoft.SqlServer.Management.Smo.Server') $Server
+[string]$nm = $s.Name
+[string]$mode = $s.Settings.LoginMode
+
+# Change to Mixed Mode
+$s.Settings.LoginMode = [Microsoft.SqlServer.Management.SMO.ServerLoginMode] $AuthMode
+$s.Alter()
+
+# Restart SQL Server to apply changes
+Restart-Service -Name MSSQLSERVER
